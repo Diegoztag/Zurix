28acbe93f3432306dd0536d4d1ac627cd528ee3c
Diego Zazueta <juniordza@gmail.com>
2016-04-21 03:42:42 -0600

    Page v0.6
    Soporte para múltiples pantallas (RWD).

--- js/script.js
+++ js/script.js
@@ -1,52 +1,19 @@
-//Script para mostrar el menu en moviles------------------
-$(document).ready(function () {
-	var flag = 1;
-	var menuWidth = $('.nav').width();
-	var abrirMenu = $('.nav');
-	var wrap = $('.wrap');
-
-	$('.menu__abrir').on('click',function () {
-		if (flag == 1) {
-			abrirMenu.animate({left: 0},300);
-			wrap.animate({opacity: 0.9},200);
-			flag = flag + 1;
-		}else{
-			abrirMenu.animate({left: - menuWidth},300);
-			wrap.animate({opacity: 1},200);
-			flag = 1;
-		}
-	});
-
-});
-
-//Script desplazamiento del nav principal-----------------
+// Script desplazamiento del nav principal-----------------
 $(document).ready(function () {
 	$('.caract').on('click',function () {
-		$('html,body').animate({scrollTop: $(".main").offset().top}, 1000);
+		$('html,body').animate({scrollTop: $(".caracter").offset().top}, 1000);
 	});
 
 	$('.acerca').on('click',function () {
 		$('html,body').animate({scrollTop: $(".about").offset().top}, 1000);
 	});
 
-	$('.desc').on('click',function () {
-		$('html,body').animate({scrollTop: $(".descarga").offset().top}, 1000);
-	});
-
-	$('.estruc').on('click',function () {
-		$('html,body').animate({scrollTop: $(".estructura").offset().top}, 1000);
+	$('.init').on('click',function () {
+		$('html,body').animate({scrollTop: $(".iniciando").offset().top}, 1000);
 	});
 
 	$('.grid').on('click',function () {
 		$('html,body').animate({scrollTop: $(".rejilla").offset().top}, 1000);
-	});
-
-	$('.desplazamiento').on('click',function () {
-		$('html,body').animate({scrollTop: $(".desplaza").offset().top}, 1000);
-	});
-
-	$('.responsive').on('click',function () {
-		$('html,body').animate({scrollTop: $(".respon").offset().top}, 1000);
 	});
 });
 
