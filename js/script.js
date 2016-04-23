e6163cdfdb1eea2ac277cfca2091c0971fa079ec
Diego Zazueta <juniordza@gmail.com>
2016-04-23 02:18:11 -0600

    Page v0.7
    Footer responsive, menú fixed añadido y botón ir arriba.

--- js/script.js
+++ js/script.js
@@ -24,4 +24,42 @@ $(document).ready(function () {
 		$('html,body').animate({scrollTop: $(".main").offset().top}, 700);
 	});
 });
+
+//menu fixed------------------------------
+
+$(document).ready(function() {
+
+	$(window).on('scroll',function(){
+
+		var altoHeader = $('.head-main').height();
+
+		if ($(window).scrollTop() < (altoHeader)) {
+			$('.menu__fix2').slideUp(300);
+		}else{
+			$('.menu__fix2').slideDown(300);
+			$('.menu__fix2').css({'visibility':'visible'});
+
+		}		
+	});
+});
 	
+//boton ir arriba------------------------------
+
+$(document).ready(function() {
+
+	var altoHeader = $('.head-main').height();
+
+	$('.btn-subir').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 1000);
+	});
+ 
+	$(window).scroll(function(){
+		if( $(window).scrollTop() > (altoHeader)){
+			$('.btn-subir').fadeIn(500);
+		} else {
+			$('.btn-subir').fadeOut(500);
+		}
+	});
+});
